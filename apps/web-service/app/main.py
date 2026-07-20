983ad02806421b61369f6b9761b1eee352841585
sunny <sunny@bytespark.app>
2026-07-20 14:57:32 +0800

    中间件

--- apps/web-service/app/main.py
+++ apps/web-service/app/main.py
@@ -3,6 +3,8 @@ from fastapi.exceptions import RequestValidationError
 from starlette.exceptions import HTTPException
 
 from app.core.config import common_settings, web_settings
+from app.core.middleware import register_middleware
+from app.core.openapi import setup_openapi
 from app.exception.handler.docs import generate_error_docs
 from app.exception.handler.handlers import exception_handler
 
@@ -16,6 +18,12 @@ app = FastAPI(
     ),
 )
 
+# 注册中间件
+register_middleware(app)
+
+# 设置 openapi 文档格式
+setup_openapi(app)
+
 # 注册全局异常处理器（覆盖 Starlette/FastAPI 内置处理器 + 兜底未知异常）
 app.add_exception_handler(RequestValidationError, exception_handler)
 app.add_exception_handler(HTTPException, exception_handler)
@@ -23,8 +31,8 @@ app.add_exception_handler(Exception, exception_handler)
 
 
 # 注册路由
-from app.api.products import router as product_router
 from app.api.categories import router as category_router
+from app.api.products import router as product_router
 from app.api.skus import router as sku_router
 
 app.include_router(product_router)
